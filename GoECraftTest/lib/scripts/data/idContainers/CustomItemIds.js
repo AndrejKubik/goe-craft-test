c2a63e975b0591a533b26ca14a5746261f38eb24
AndrejKubik <andrejkk97@gmail.com>
2026-05-27 17:00:47 +0200

    added fertilizer and uber fertilizer items

--- GoECraftTest/lib/scripts/data/idContainers/CustomItemIds.js
+++ GoECraftTest/lib/scripts/data/idContainers/CustomItemIds.js
@@ -1,5 +1,9 @@
 export const CustomItemIds = {
     tomatoSeed: "tomato_seed",
     cucumberSeed: "cucumber_seed",
+    tomato: "tomato",
+    cucumber: "cucumber",
+    fertilizer: "fertilizer",
+    uberFertilizer: "uber_fertilizer",
 };
 //# sourceMappingURL=CustomItemIds.js.map
--- GoECraftTest/scripts/data/idContainers/CustomItemIds.ts
+++ GoECraftTest/scripts/data/idContainers/CustomItemIds.ts
@@ -1,4 +1,8 @@
 export const CustomItemIds = {
   tomatoSeed: "tomato_seed",
   cucumberSeed: "cucumber_seed",
+  tomato: "tomato",
+  cucumber: "cucumber",
+  fertilizer: "fertilizer",
+  uberFertilizer: "uber_fertilizer",
 } as const;
